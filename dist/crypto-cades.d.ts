a5b21481e2aaaf2b11748caa9c4a0da7b3b0f46b
Dmitry Ostanin <dmitry@ostanin.email>
2022-03-27 05:55:26 +0300

    Сборка

---
+++ dist/crypto-cades.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for crypto-cades 0.0.1
+// Project: crypto-cades
+// Definitions by: undefined undefined
+
+export as namespace cryptoPro;
+
+export * from './api';
---
+++ lib/crypto-cades.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for crypto-cades 0.0.1
+// Project: crypto-cades
+// Definitions by: undefined undefined
+
+export * from './api';
